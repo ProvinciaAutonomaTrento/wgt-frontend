22f73f86cab9d2ed1f9cfe79be054f40495e3151
Trilogis <info@trilogis.it>
2017-10-24 16:54:18 +0200

    Removed unused document-popup code and added 'ga-tooltip' class to carousel-popup.

--- custom_src/components/tooltip/TooltipDirective.js
+++ custom_src/components/tooltip/TooltipDirective.js
@@ -553,9 +553,6 @@ goog.require('ga_urlutils_service');
                                     openImmaginiPopup: function (layer, feature, lang) {
                                         getCarousel(layer, feature, lang);
                                     },
-                                    // openDocumentiPopup: function (layer, feature, lang) {
-                                    //     getDocuments(layer, feature, lang);
-                                    // },
                                     onCloseCallback: function () {
                                         if (onCloseCB) {
                                             onCloseCB();
@@ -613,24 +610,9 @@ goog.require('ga_urlutils_service');
                         });
                     };
 
-                    // var getDocuments = function (layer, feature, lang) {
-                    //     var urlReq = scope.options.documentsUrlTemplate;
-                    //     $http.get(urlReq, {
-                    //         params: {
-                    //             layer: layer,
-                    //             feature: feature,
-                    //             lang: lang
-                    //         },
-                    //         headers: {
-                    //             'Accept': 'text/html'
-                    //         }
-                    //     }).success(function (html) {
-                    //         showDocuments(html);
-                    //     });
-                    // };
-
                     var showCarousel = function (html) {
                         var carouselPopup = gaPopup.create({
+                            className: 'ga-tooltip',
                             title: 'object_images',
                             content: html
                         });
@@ -643,14 +625,6 @@ goog.require('ga_urlutils_service');
                             });
                         }
                     };
-
-                    // var showDocuments = function (html) {
-                    //     var documentsPopup = gaPopup.create({
-                    //         title: 'object_documents',
-                    //         content: html
-                    //     });
-                    //     documentsPopup.open();
-                    // };
 
                 // end link param
                 }
